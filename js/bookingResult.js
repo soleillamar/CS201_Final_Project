185a80e0279fcd857a10d8be67a30ac0d5e5559b
soleillamar <soleillamar@gmail.com>
2021-05-16 17:14:47 +1000

    jquery add

--- js/bookingResult.js
+++ js/bookingResult.js
@@ -6,5 +6,7 @@ $(document).ready(function(){
 function loadBookingData(){
   const finalBookingResult = JSON.parse(localStorage.getItem("booking-data"));
   $("#complete-name").html(finalBookingResult["first_name"] + " " + finalBookingResult["last_name"]);
-  $("#booking-dates").html(finalBookingResult["start_date"] + " - " + finalBookingResult["end_date"]);
+  $("#booking-dates").html("Your Stay:" + " " + finalBookingResult["start_date"] + " - " + finalBookingResult["end_date"]);
+  $("#email").html("Your Email:" + " " + finalBookingResult["email"]);
+  $('#number-beds option:selected').text();
 }
--- js/myScript.js
+++ js/myScript.js
@@ -5,12 +5,11 @@ let booking = {
   "email" : "",
   "start_date": "",
   "end_date": "",
-  "amenities" : {
-    "number_beds" : 1,
-    "type_beds" : "King",
-    "breakfast" : true,
-    "pool" : false
-  }
+  "number_beds" : 1,
+  "type_beds" : "King",
+  "breakfast" : true,
+  "pool" : false
+
 };
 
 let bookingTemp = JSON.parse(localStorage.getItem("booking-data"));//assumption booking data will be stored as a string
@@ -23,7 +22,6 @@ if (!bookingTemp){
 }
 
 $(document).ready(function(){
-  $("#myName").html("<h2>Enter Booking Info Below</h2>");
 
   $("#first-name").blur(function(){
     booking.first_name = $("#first-name").val();
@@ -45,7 +43,11 @@ $(document).ready(function(){
     console.log(localStorage.getItem("booking-data"));
     console.log(JSON.parse(localStorage.getItem("booking-data")));
   });
-
+$("#number-beds").selectmenu();
+$("#type-beds").selectmenu();
+$("#number-bathrooms").selectmenu();
+$(".food").checkboxradio();
+$(".clean").checkboxradio();
 });
 
 function submitBooking(){
@@ -57,11 +59,51 @@ function submitBooking(){
     }
   });
 
-}
+};
+
 function runFormValidations(){
   console.log("running form validations");
   //todo write the form validations
-  return true;// return true if no errors -- run false if errors
-}
-  $(number-beds).selectmenu();
-  $(type-beds).selectmenu();
+  areFormErrors = false;
+  errorMessages = "";
+  errorMessages += "<ul>"; // return true if no errors -- run false if errors
+
+  //check first and last name
+  if($("#first-name").val().length === 0) {
+        errorMessages += "<li>Missing first name.</li>";
+        $("#first-name").addClass("error");
+        areFormErrors = true;
+    }
+  if($("#last-name").val().length === 0) {
+        errorMessages += "<li>Missing last name.</li>";
+        $("#last-name").addClass("error");
+        areFormErrors = true;
+    }
+  //check Emails
+  var emailPattern =  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+  if($("#email").val().length === 0 || !emailPattern.test($("#email").val())) {
+      errorMessages += "<li>Invalid or missing email address.</li>";
+      $("#email").addClass("error");
+      areFormErrors = true;
+    }
+  //check Dates
+  if($("#start-date").val().length === 0) {
+        errorMessages += "<li>Missing start date.</li>";
+        $("#start-date").addClass("error");
+    }
+  if($("#end-date").val().length === 0) {
+        errorMessages += "<li>Missing end date.</li>";
+        $("#end-date").addClass("error");
+        areFormErrors = true;
+    }
+  
+  errorMessages += "</ul>";
+    if(areFormErrors) {
+      document.getElementById("formErrors").innerHTML = errorMessages;
+      document.getElementById("formErrors").style.display = "block";
+      return false;
+    } else {
+        document.getElementById("formErrors").style.display = "none";
+        return true;
+    };
+  };
